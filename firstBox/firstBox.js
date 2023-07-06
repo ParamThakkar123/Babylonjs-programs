f0bb431c46ebd3ecf26171e63ad71a2758b1ef10
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-07-07 00:23:38 +0530

    Update firstBox.js

--- firstBox/firstBox.js
+++ firstBox/firstBox.js
@@ -4,22 +4,21 @@ const canvas = document.getElementById('renderCanvas');
 
 const engine = new BABYLON.Engine(canvas);
 
-var createScene = function () {
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
-
-    engine.runRenderLoop(function(){
-        scene.render();
-    })
+const createScene = function() {
+    const scene = new BABYLON.Scene(engine);
 
     scene.createDefaultCameraOrLight(true, false, true);
-    const box = BABYLON.MeshBuilder.CreateBox();
+    const box = new BABYLON.MeshBuilder.CreateBox();
 
     return scene;
-};
+}
 
 const scene = createScene();
 
 engine.runRenderLoop(function() {
     scene.render();
 });
+
+window.addEventListener('resize', function() {
+    engine.resize();
+})
